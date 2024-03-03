0c87851ba54b18e70c49ab366440b539881f5b96
ravigod22 <ravi22135@gmail.com>
2024-03-03 15:10:00 -0500

    plantilla v1.1

---
+++ plantilla.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
